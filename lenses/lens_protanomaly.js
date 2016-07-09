f22b46b83e1484ecf4681413fab8bd54553184f9
eugenecys <eugenecys@gmail.com>
2016-07-10 06:59:53 +0800

    Added rest of colors

--- lenses/lens_protanomaly.js
+++ lenses/lens_protanomaly.js
@@ -4,7 +4,7 @@ var iDiv = document.createElement('div');
 iDiv.id = 'colorFilters';
 iDiv.innerHTML = '<svg id="colorFilterSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" baseProfile="full"> ' +
   '<filter id="protanomaly"> ' +
-  '<feColorMatrix type="matrix" values="0.817,0.183,0,0,0 0.333,0.667,0,0,0 0,0.125,0.875,0,0 0,0,0,1,0" in="SourceGraphic" /> ' +
+  '<feColorMatrix type="matrix" values="0.817,0.183,0,0,0,0.333,0.667,0,0,0,0,0.125,0.875,0,0,0,0,0,1,0" in="SourceGraphic" /> ' +
   '</filter> ' +
   '</svg>';
 
--- lenses/lens_tritanomaly.js
+++ lenses/lens_tritanomaly.js
@@ -3,11 +3,11 @@ console.log('content script loaded')
 var iDiv = document.createElement('div');
 iDiv.id = 'colorFilters';
 iDiv.innerHTML = '<svg id="colorFilterSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" baseProfile="full"> ' +
-  '<filter id="deuteranopia"> ' +
-  '<feColorMatrix type="matrix" values="0.625,0.375,0,0,0,0.7,0.3,0,0,0,0,0.3,0.7,0,0,0,0,0,1,0" in="SourceGraphic" /> ' +
+  '<filter id="tritanomaly"> ' +
+  '<feColorMatrix type="matrix" values="0.967,0.033,0,0,0,0,0.733,0.267,0,0,0,0.183,0.817,0,0,0,0,0,1,0" in="SourceGraphic" /> ' +
   '</filter> ' +
   '</svg>';
 
 document.body.appendChild(iDiv);
-document.body.style.filter = "url(#deuteranopia)";
-document.body.style.webkitFilter = "url(#deuteranopia)";
+document.body.style.filter = "url(#tritanomaly)";
+document.body.style.webkitFilter = "url(#tritanomaly)";
