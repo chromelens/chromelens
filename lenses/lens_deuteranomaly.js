3343fb8e47f96a03a3ab50fad2fcaed02cc761d8
eugenecys <eugenecys@gmail.com>
2016-07-10 07:25:10 +0800

    Fixed bug

--- lenses/lens_deuteranomaly.js
+++ lenses/lens_deuteranomaly.js
@@ -3,11 +3,11 @@ console.log('content script loaded')
 var iDiv = document.createElement('div');
 iDiv.id = 'colorFilters';
 iDiv.innerHTML = '<svg id="colorFilterSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" baseProfile="full"> ' +
-  '<filter id="deuteranopia"> ' +
+  '<filter id="deuteranomaly"> ' +
   '<feColorMatrix type="matrix" values="0.8,0.2,0,0,0,0.258,0.742,0,0,0,0,0.142,0.858,0,0,0,0,0,1,0" in="SourceGraphic" /> ' +
   '</filter> ' +
   '</svg>';
 
 document.body.appendChild(iDiv);
-document.body.style.filter = "url(#deuteranopia)";
-document.body.style.webkitFilter = "url(#deuteranopia)";
+document.body.style.filter = "url(#deuteranomaly)";
+document.body.style.webkitFilter = "url(#deuteranomaly)";
