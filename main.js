0d9ab0e237281089be3bfca844f1de0b89d81f30
Jingwen Chen <jin@crypt.sg>
2016-07-09 14:21:04 -0700

    dom access

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ function log(s) {
   chrome.devtools.inspectedWindow.eval('console.log(' + string + ')')
 };
 
+var backgroundPageConnection = chrome.runtime.connect();
+backgroundPageConnection.onMessage.addListener(function (message) {
+   // Handle responses from the background page, if any
+});
+
+// Relay the tab ID to the background page
+chrome.runtime.sendMessage({
+   tabId: chrome.devtools.inspectedWindow.tabId,
+   scriptToInject: "content_script.js"
+});
+
 // Create panel
 function createPanel() {
   chrome.devtools.panels.create('ChromeLens', '', 'panel.html', function(panel) {
