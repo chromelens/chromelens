9cf17bf22d9dbe1ff62d8e153c45a99f82055d84
Jingwen Chen <jin@crypt.sg>
2016-07-09 19:42:20 -0700

    Initial implementation of full blindness

--- lenses/lens_fullblindness.js
+++ lenses/lens_fullblindness.js
@@ -1,4 +1,21 @@
 console.log('content script loaded')
 
-document.body.style.filter = "brightness(0%)";
-document.body.style.webkitFilter = "brightness(0%)";
+delete document.body.style.filter;
+delete document.body.style.webkitFilter;
+
+const styles = `
+  background-color: black;
+  pointer-events: none;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  z-index: 9999999;
+`
+
+var iDiv = document.createElement('div');
+iDiv.id = 'colorFilters';
+iDiv.innerHTML = `<div style="${styles}"></div>`;
+
+document.body.appendChild(iDiv);
