98b9dd6d58ff1c33742d1ad96ef99d1c12018323
Zhi An Ng <zhian@uber.com>
2016-07-09 17:25:53 -0700

    Prettier results

--- run_axs.js
+++ run_axs.js
@@ -1,34 +1,56 @@
-var run_result = axs.Audit.run();
-STYLE = ''
+var STYLE =
+  '#chrome-lens-base {' +
+  '  position: absolute;' +
+  '  top: 0;' +
+  '  left: 0;' +
+  '  z-index: 999999;' +
+  '}' +
+  '.chrome-lens-warning {' +
+  '  position: absolute;' +
+  '  box-shadow: 0 0 4px 4px #f7983a;' +
+  '  border-radius: 2px;' +
+  '}';
 
-function addStyle() {
+var CHROME_LENS_STYLE_ID = 'chrome-lens-style'
+var CHROME_LENS_BASE_ID = 'chrome-lens-base'
+var CHROME_LENS_WARNING_CLASS = 'chrome-lens-warning'
+
+function initDom() {
+  if (document.querySelector('#' + CHROME_LENS_STYLE_ID)) return;
+
   var style = document.createElement('style');
-  style.id = 'chrome_lens_style';
-  style.innerHTML = STYLE
-  cvox.DomUtil.addNodeToHead(style, style.id);
-}
+  style.id = CHROME_LENS_STYLE_ID;
+  style.innerHTML = STYLE;
+  document.head.appendChild(style)
 
-function buildBase() {
+  if (document.querySelector('#' + CHROME_LENS_BASE_ID)) return;
+
   const div = document.createElement('div');
+  div.id = CHROME_LENS_BASE_ID;
   document.body.appendChild(div);
 }
 
 function indicate(el, rule_violated) {
-    const {top, right, bottom, left, width, height, x, y} = el.getBoundingClientRect()
-    const {code, heading, name, severity, url} = rule_violated;
+  console.log(el.getBoundingClientRect());
+  const {top, right, bottom, left, width, height, x, y} = el.getBoundingClientRect()
+  const {code, heading, name, severity, url} = rule_violated;
 
-    el.style.backgroundColor = 'red';
-    console.log(el + ' failed ' + rule_violated)
+  // el.style.backgroundColor = 'red';
+  console.log(el + ' failed ' + rule_violated)
 
-    // div = document.createElement('div');
+  const div = document.createElement('div');
+  div.className = CHROME_LENS_WARNING_CLASS;
 
-    // div.style.position = 'absolute';
-    // div.style.left = left;
-    // div.style.width = width;
-    // div.style.height = height;
-    // div.style.borderWidth = '10px';
-    // div.style.borderColor = 'red';
+  div.style.left = left + 'px';
+  div.style.top = top + 'px';
+  div.style.width = width + 'px';
+  div.style.height = height + 'px';
+  const base = document.querySelector('#' + CHROME_LENS_BASE_ID);
+  base.appendChild(div);
 }
+
+var run_result = axs.Audit.run();
+initDom()
 
 run_result.forEach(function(v) {
   if (v.result !== 'FAIL') {
